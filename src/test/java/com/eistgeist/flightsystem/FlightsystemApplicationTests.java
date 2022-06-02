8b31d54b6ad4859592a1ad6684776e0210bdf203
zaqim <hdegirmenci18@ku.edu.tr>
2022-06-02 19:01:24 +0200

    Initial commit

---
+++ src/test/java/com/eistgeist/flightsystem/FlightsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eistgeist.flightsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
