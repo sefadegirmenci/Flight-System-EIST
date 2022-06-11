1c3db73914b26053dde398360552bda7c3738091
zaqim <hdegirmenci18@ku.edu.tr>
2022-06-11 16:43:20 +0200

    Add service layer

--- src/main/java/com/eistgeist/flightsystem/service/FlightService.java
+++ src/main/java/com/eistgeist/flightsystem/service/FlightService.java
@@ -1,2 +1,24 @@
-package com.eistgeist.flightsystem.service;public class FlightService {
+package com.eistgeist.flightsystem.service;
+
+import com.eistgeist.flightsystem.model.Flight;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class FlightService {
+
+    private List<Flight> flights=new ArrayList<>();
+
+    public List<Flight> getFlights(){
+        return flights;
+    }
+
+    public Flight addFlight(Flight flight){
+        this.flights.add(flight);
+        return flight;
+    }
+
+
 }
