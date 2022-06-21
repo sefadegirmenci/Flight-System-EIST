c0158ff5c0557e993fa9bbb48e3cae3b133185f5
zaqim <hdegirmenci18@ku.edu.tr>
2022-06-21 21:32:14 +0200

    Query flights by arrival and departure airports

--- src/main/java/com/eistgeist/flightsystem/repository/FlightRepository.java
+++ src/main/java/com/eistgeist/flightsystem/repository/FlightRepository.java
@@ -1,7 +1,12 @@
 package com.eistgeist.flightsystem.repository;
 
+import com.eistgeist.flightsystem.model.Airport;
 import com.eistgeist.flightsystem.model.Flight;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface FlightRepository extends MongoRepository<Flight,String> {
+import java.util.Optional;
+
+public interface FlightRepository
+        extends MongoRepository<Flight,String> {
+    Optional<Flight> findFlightsByArrivalAirportIgnoreCaseAndDepartureAirportIgnoreCase(Airport arrivalAirport, Airport departureAirport);
 }
