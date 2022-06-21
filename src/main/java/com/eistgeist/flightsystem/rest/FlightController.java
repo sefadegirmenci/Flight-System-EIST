37c2185dbcde91d66350dfeb57b9e18c904eb122
zaqim <hdegirmenci18@ku.edu.tr>
2022-06-21 21:28:57 +0200

    Search flight by departure and arrival airport

--- src/main/java/com/eistgeist/flightsystem/rest/FlightController.java
+++ src/main/java/com/eistgeist/flightsystem/rest/FlightController.java
@@ -1,7 +1,9 @@
 package com.eistgeist.flightsystem.rest;
 
+import com.eistgeist.flightsystem.model.Airport;
 import com.eistgeist.flightsystem.model.Flight;
 import com.eistgeist.flightsystem.service.FlightService;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,22 +13,22 @@ import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/")
+@AllArgsConstructor
 public class FlightController {
     private final FlightService flightService;
-
-    @Autowired
-    public FlightController(FlightService flightService) {
-        this.flightService = flightService;
-    }
 
     @GetMapping("flights")
     public ResponseEntity<List<Flight>> getFlights(){
         return ResponseEntity.ok(flightService.getFlights());
     }
 
-    @PostMapping("flights")
+    /*@PostMapping("flights")
     public ResponseEntity<Flight> addFlight(@RequestBody Flight flight){
         return ResponseEntity.ok(flightService.addFlight(flight));
+    }*/
+    @GetMapping("searchFlight")
+    public ResponseEntity<Flight> searchFlight(Airport departurAirport, Airport arrivalAirport) {
+        return ResponseEntity.ok(flightService.searchFlight(departurAirport, arrivalAirport));
     }
 
 }
