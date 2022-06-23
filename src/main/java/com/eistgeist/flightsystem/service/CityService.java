751b57078f03744da58eebb2c7ddd3e24199c782
zaqim <hdegirmenci18@ku.edu.tr>
2022-06-23 21:25:56 +0200

    Implement city service

---
+++ src/main/java/com/eistgeist/flightsystem/service/CityService.java
@@ -0,0 +1,19 @@
+package com.eistgeist.flightsystem.service;
+
+import com.eistgeist.flightsystem.model.City;
+import com.eistgeist.flightsystem.repository.CityRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class CityService {
+    private CityRepository cityRepository;
+    public List<City> getCities() {
+        return cityRepository.findAll();
+    }
+
+    /* TODO: Add map and POI */
+}
