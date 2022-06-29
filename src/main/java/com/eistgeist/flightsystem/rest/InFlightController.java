2a5ea89ee0e7323d373fe8b7f7cca6e3a5b88464
zaqim <hdegirmenci18@ku.edu.tr>
2022-06-29 21:20:36 +0200

    Add InFlightController

---
+++ src/main/java/com/eistgeist/flightsystem/rest/InFlightController.java
@@ -0,0 +1,11 @@
+package com.eistgeist.flightsystem.rest;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+public class InFlightController {
+
+}
