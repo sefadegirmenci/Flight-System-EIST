0e8f02c32ff624f00d25a517b9cbe143bc625d74
zaqim <hdegirmenci18@ku.edu.tr>
2022-06-29 21:18:14 +0200

    Add InFlightService

--- src/main/java/com/eistgeist/flightsystem/service/InFlightService.java
+++ src/main/java/com/eistgeist/flightsystem/service/InFlightService.java
@@ -1,7 +1,5 @@
 package com.eistgeist.flightsystem.service;
 
-import com.eistgeist.flightsystem.model.Instruction;
-import com.eistgeist.flightsystem.repository.FlightRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
