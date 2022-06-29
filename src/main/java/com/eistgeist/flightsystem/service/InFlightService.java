ca7491f436781efeaf1705746214459449669244
zaqim <hdegirmenci18@ku.edu.tr>
2022-06-29 21:16:34 +0200

    Add InFlightService

---
+++ src/main/java/com/eistgeist/flightsystem/service/InFlightService.java
@@ -0,0 +1,14 @@
+package com.eistgeist.flightsystem.service;
+
+import com.eistgeist.flightsystem.model.Instruction;
+import com.eistgeist.flightsystem.repository.FlightRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class InFlightService {
+
+
+
+}
