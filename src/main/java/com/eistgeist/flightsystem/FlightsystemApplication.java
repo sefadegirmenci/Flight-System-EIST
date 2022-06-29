61df52ed860bba89e74d81ef56e92ac498208cc7
zaqim <hdegirmenci18@ku.edu.tr>
2022-06-29 21:08:34 +0200

    Add a flight via command line runner

--- src/main/java/com/eistgeist/flightsystem/FlightsystemApplication.java
+++ src/main/java/com/eistgeist/flightsystem/FlightsystemApplication.java
@@ -1,7 +1,12 @@
 package com.eistgeist.flightsystem;
 
+import com.eistgeist.flightsystem.model.Airport;
+import com.eistgeist.flightsystem.model.Flight;
+import com.eistgeist.flightsystem.repository.FlightRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class FlightsystemApplication {
@@ -9,5 +14,15 @@ public class FlightsystemApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(FlightsystemApplication.class, args);
 	}
-
+	@Bean
+	CommandLineRunner runner(FlightRepository repository) {
+		return args -> {
+			Flight flight = new Flight(
+					55,
+					new Airport("IST","Istanbul"),
+					new Airport("MC","München")
+			);
+			repository.insert(flight);
+		};
+	}
 }
