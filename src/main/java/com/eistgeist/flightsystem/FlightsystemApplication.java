2be508ac2367749407f620c2b0382c9850931fe9
zaqim <hdegirmenci18@ku.edu.tr>
2022-07-06 22:20:36 +0200

    Add CORS mapping

--- src/main/java/com/eistgeist/flightsystem/FlightsystemApplication.java
+++ src/main/java/com/eistgeist/flightsystem/FlightsystemApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class FlightsystemApplication {
@@ -14,5 +16,13 @@ public class FlightsystemApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(FlightsystemApplication.class, args);
 	}
-
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/").allowedOrigins("http://localhost:4200");
+			}
+		};
+	}
 }
