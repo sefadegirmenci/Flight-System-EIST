8ee9521897349d5bf65c581dbe4a7d69fb81c023
zaqim <hdegirmenci18@ku.edu.tr>
2022-07-07 21:14:26 +0200

    Add ItemNotFound exception

---
+++ src/main/java/com/eistgeist/flightsystem/exception/ItemNotFoundException.java
@@ -0,0 +1,8 @@
+package com.eistgeist.flightsystem.exception;
+
+public class ItemNotFoundException extends RuntimeException{
+
+    public ItemNotFoundException(String msg) {
+        super(msg);
+    }
+}
