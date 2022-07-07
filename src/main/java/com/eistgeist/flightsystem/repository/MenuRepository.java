8e56d0acb0c8d100c28ad5241cf8ead76d673ce9
zaqim <hdegirmenci18@ku.edu.tr>
2022-07-07 20:12:30 +0200

    Delete menu classes

--- src/main/java/com/eistgeist/flightsystem/repository/MenuRepository.java
+++
@@ -1,7 +0,0 @@
-package com.eistgeist.flightsystem.repository;
-
-import com.eistgeist.flightsystem.model.Item;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface MenuRepository extends MongoRepository<Item,String> {
-}
