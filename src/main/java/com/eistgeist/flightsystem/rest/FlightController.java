18d2db32ef681c8bb5f1ad6326e59ac32abc184e
zaqim <hdegirmenci18@ku.edu.tr>
2022-07-07 15:54:15 +0200

    Add openapi descriptions

--- src/main/java/com/eistgeist/flightsystem/rest/FlightController.java
+++ src/main/java/com/eistgeist/flightsystem/rest/FlightController.java
@@ -3,6 +3,7 @@ package com.eistgeist.flightsystem.rest;
 import com.eistgeist.flightsystem.model.Airport;
 import com.eistgeist.flightsystem.model.Flight;
 import com.eistgeist.flightsystem.service.FlightService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -19,15 +20,20 @@ import java.util.List;
 public class FlightController {
     private final FlightService flightService;
 
+    @Operation(summary = "Get all the flights")
     @GetMapping("flights")
     public ResponseEntity<List<Flight>> getFlights(){
         return ResponseEntity.ok(flightService.getFlights());
     }
 
+    @Operation(summary = "Put a flight")
     @PostMapping("flights")
     public ResponseEntity<Flight> addFlight(@RequestBody Flight flight){
         return new ResponseEntity<>(flightService.addFlight(flight), HttpStatus.CREATED);
     }
+
+    /* TODO: Change the search logic*/
+    @Operation(summary = "Search a flight by departure and arrival airports")
     @GetMapping(value = "search/{departureAirport}/{arrivalAirport}",produces = {"application/json"})
     public ResponseEntity<Flight> searchFlight(@PathVariable Airport departureAirport, @PathVariable Airport arrivalAirport) {
         return ResponseEntity.ok(flightService.searchFlight(departureAirport, arrivalAirport));
