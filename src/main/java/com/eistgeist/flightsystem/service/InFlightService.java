52e5eb99908f4791bc98fac709e6d54925079a9f
zaqim <hdegirmenci18@ku.edu.tr>
2022-07-07 20:12:00 +0200

    Add service layer of the menu

--- src/main/java/com/eistgeist/flightsystem/service/InFlightService.java
+++ src/main/java/com/eistgeist/flightsystem/service/InFlightService.java
@@ -1,12 +1,21 @@
 package com.eistgeist.flightsystem.service;
 
+import com.eistgeist.flightsystem.model.Item;
+import com.eistgeist.flightsystem.repository.InFlightRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @AllArgsConstructor
 public class InFlightService {
+    private InFlightRepository inFlightRepository;
 
-
-
+    public List<Item> getItems() {
+        return inFlightRepository.findAll();
+    }
+    public Item addItem(Item item) {
+        return inFlightRepository.save(item);
+    }
 }
