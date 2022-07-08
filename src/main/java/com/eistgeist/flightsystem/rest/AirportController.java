2d57357df867e2a07db9b72a4b72065fc59d27fc
zaqim <hdegirmenci18@ku.edu.tr>
2022-07-08 18:55:07 +0200

    Change AirportController to rest controller

--- src/main/java/com/eistgeist/flightsystem/rest/AirportController.java
+++ src/main/java/com/eistgeist/flightsystem/rest/AirportController.java
@@ -5,12 +5,9 @@ import com.eistgeist.flightsystem.service.AirportService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 @AllArgsConstructor
 @CrossOrigin
 @RequestMapping("airport")
