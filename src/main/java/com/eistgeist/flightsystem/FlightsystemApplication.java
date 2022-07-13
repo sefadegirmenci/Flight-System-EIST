c8a62874374715bb1c5e2e63747d5633d17af61d
zaqim <hdegirmenci18@ku.edu.tr>
2022-07-14 00:23:25 +0200

    fix springfox

--- src/main/java/com/eistgeist/flightsystem/FlightsystemApplication.java
+++ src/main/java/com/eistgeist/flightsystem/FlightsystemApplication.java
@@ -8,9 +8,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
+@EnableWebMvc
 public class FlightsystemApplication {
 
 	public static void main(String[] args) {
@@ -25,4 +27,5 @@ public class FlightsystemApplication {
 			}
 		};
 	}*/
+
 }
