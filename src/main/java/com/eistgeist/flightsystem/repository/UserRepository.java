a6eada2a17c23ed06d1db51bbac994b88b4cc995
zaqim <hdegirmenci18@ku.edu.tr>
2022-07-14 12:54:47 +0200

    Find user by username

--- src/main/java/com/eistgeist/flightsystem/repository/UserRepository.java
+++ src/main/java/com/eistgeist/flightsystem/repository/UserRepository.java
@@ -6,4 +6,5 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import java.util.Optional;
 
 public interface UserRepository extends MongoRepository<User,String> {
+    Optional<User> findDistinctByUserByUserNameIgnoreCase(String userName);
 }
