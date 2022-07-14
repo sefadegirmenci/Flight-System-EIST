295bace06b7abe6a88465af5bd9e588a38d18c63
zaqim <hdegirmenci18@ku.edu.tr>
2022-07-14 13:44:13 +0200

    Add a constructor for the list

--- src/main/java/com/eistgeist/flightsystem/model/JourneyList.java
+++ src/main/java/com/eistgeist/flightsystem/model/JourneyList.java
@@ -2,11 +2,16 @@ package com.eistgeist.flightsystem.model;
 
 import lombok.Data;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Data
 public class JourneyList {
     private List<Journey> journeyList;
+
+    public JourneyList() {
+        this.journeyList = new ArrayList<>();
+    }
 
     public boolean insertJourney(Journey journey) {
         return journeyList.add(journey);
