da6ad40dc6647c723c2996dcfeb03cc4dcb5704a
zaqim <hdegirmenci18@ku.edu.tr>
2022-07-21 16:45:11 +0200

    Add fields to poi class

--- src/main/java/com/eistgeist/flightsystem/model/POI.java
+++ src/main/java/com/eistgeist/flightsystem/model/POI.java
@@ -1,4 +1,13 @@
 package com.eistgeist.flightsystem.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class POI {
+    double xCoordinate;
+    double yCoordinate;
+
+    public POI(){}
 }
